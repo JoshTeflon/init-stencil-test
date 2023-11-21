e4dab95e3011b70d19c2083099ae01d564f04c55
JoshTeflon <jenikele21h@gmail.com>
2023-11-21 16:29:01 +0100

    render test component

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,6 +6,8 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
+    interface ClockComponent {
+    }
     interface MyComponent {
         /**
           * The first name
@@ -22,6 +24,12 @@ export namespace Components {
     }
 }
 declare global {
+    interface HTMLClockComponentElement extends Components.ClockComponent, HTMLStencilElement {
+    }
+    var HTMLClockComponentElement: {
+        prototype: HTMLClockComponentElement;
+        new (): HTMLClockComponentElement;
+    };
     interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
     }
     var HTMLMyComponentElement: {
@@ -29,10 +37,13 @@ declare global {
         new (): HTMLMyComponentElement;
     };
     interface HTMLElementTagNameMap {
+        "clock-component": HTMLClockComponentElement;
         "my-component": HTMLMyComponentElement;
     }
 }
 declare namespace LocalJSX {
+    interface ClockComponent {
+    }
     interface MyComponent {
         /**
           * The first name
@@ -48,6 +59,7 @@ declare namespace LocalJSX {
         "middle"?: string;
     }
     interface IntrinsicElements {
+        "clock-component": ClockComponent;
         "my-component": MyComponent;
     }
 }
@@ -55,6 +67,7 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
+            "clock-component": LocalJSX.ClockComponent & JSXBase.HTMLAttributes<HTMLClockComponentElement>;
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
         }
     }
